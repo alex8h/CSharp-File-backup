d84feb6839fb6b50fe8f749b2335d5e228b4c4a5
Alex Hunter <tat7446@yandex.ru>
2020-08-11 16:43:28 +0700

    Добавьте файлы проекта.

---
+++ File-Backup/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace File_Backup
+{
+    public class JSON
+    {
+        public List<string> sourse { get; set; }
+        public string destination { get; set; }
+    }
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите путь в файлу json");
+            string path = Console.ReadLine();
+            //string path = @"C:\Users\tat74\source\repos\File-Backup\paths.json";
+            if (!File.Exists(path))
+            {
+                Console.WriteLine("Файла не существует");
+                Console.ReadKey();
+                return;
+            }
+            JSON json = JsonConvert.DeserializeObject<JSON>(File.ReadAllText(path, Encoding.Default));
+
+            string temp_path = json.destination + @"\Temp";
+
+            CreateTempDirectory(temp_path);
+
+            BackupToTemp(json.sourse, temp_path);
+
+            return;
+        }
+        static void CreateTempDirectory(string path)
+        {
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, true);
+            }
+            Directory.CreateDirectory(path);
+        }
+
+        static void CopyDir(string sourse, string destination)
+        {
+            string[] files = System.IO.Directory.GetFiles(sourse);
+
+            foreach (string s in files)
+            {
+                try
+                {
+                    string fileName = System.IO.Path.GetFileName(s);
+                    string destFile = System.IO.Path.Combine(destination, fileName);
+                    System.IO.File.Copy(s, destFile);
+                }
+                catch (ArgumentException e)
+                {
+                    Console.WriteLine("{0}: {1}, {2}", e.GetType().Name, e.Message, s);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Console.WriteLine("У приложения не разрешения к {0}", s);
+                }
+                catch (DirectoryNotFoundException e)
+                {
+                    Console.WriteLine("{0}", e.Message);
+                }
+            }
+            string[] directories = System.IO.Directory.GetDirectories(sourse);
+            foreach (string s in directories)
+            {
+                string new_path = destination  + s.Substring(s.LastIndexOf('\\'));
+                Directory.CreateDirectory(new_path);
+                CopyDir(s, new_path);
+            }
+        }
+        static void BackupToTemp(List<string> sourse, string temp)
+        {
+            foreach (string path in sourse)
+            {
+                try
+                {
+                    if (System.IO.Directory.Exists(path))
+                    {
+                        CopyDir(path, temp);
+                    }
+                    else
+                    {
+                        throw new DirectoryNotFoundException();
+                    }
+                }
+                catch(DirectoryNotFoundException)
+                {
+                    Console.WriteLine("Путь {0} не существует", path);
+                }
+                
+            }
+        }
+    }
+}
---
+++ File-Backup/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("File-Backup")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("File-Backup")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("168296fa-5e16-4db7-9b81-fae7628fea03")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
